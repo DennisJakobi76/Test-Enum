7e31954ca12b3f18daa27ce89b3262adf6f23d4b
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-12-15 17:15:01 +0100

    Interaktive Benutzerauswahl für Ampelsteuerung hinzugefügt
    Das Programm verwendet nun eine Schleife, in der der Benutzer wiederholt eine Aktion für die Ampel auswählen kann ("Stop", "Wait", "Go" oder Beenden). Nach jeder Auswahl werden die aktuelle Aktion und die zugehörige Ampelfarbe angezeigt. Ungültige Eingaben werden abgefangen und führen zu einer Fehlermeldung. Das Programm kann mit "0" beendet werden.

--- Test Enum Traffic Light/Program.cs
+++ Test Enum Traffic Light/Program.cs
@@ -4,11 +4,49 @@
     {
         static void Main(string[] args)
         {
-            TrafficLight light = new TrafficLight("Go");
+            bool running = true;
 
-            Console.WriteLine("Traffic Light Action: " + light.getAction());
-            Console.WriteLine("Traffic Light Color: " + light.getColor());
+            while (running)
+            {
+                Console.WriteLine("\n=== Traffic Light System ===");
+                Console.WriteLine("Wählen Sie eine Aktion:");
+                Console.WriteLine("1 - Stop");
+                Console.WriteLine("2 - Wait");
+                Console.WriteLine("3 - Go");
+                Console.WriteLine("0 - Programm beenden");
+                Console.Write("\nIhre Auswahl: ");
 
+                string input = Console.ReadLine();
+                string action = "";
+
+                switch (input)
+                {
+                    case "1":
+                        action = "Stop";
+                        break;
+                    case "2":
+                        action = "Wait";
+                        break;
+                    case "3":
+                        action = "Go";
+                        break;
+                    case "0":
+                        running = false;
+                        Console.WriteLine("Programm wird beendet...");
+                        continue;
+                    default:
+                        Console.WriteLine("Ungültige Eingabe! Bitte wählen Sie 0-3.");
+                        continue;
+                }
+
+                TrafficLight light = new TrafficLight(action);
+
+                Console.WriteLine("\nErgebnis:");
+                Console.WriteLine("Traffic Light Action: " + light.getAction());
+                Console.WriteLine("Traffic Light Color: " + light.getColor());
+            }
+
+            Console.WriteLine("\nDrücken Sie eine beliebige Taste zum Beenden...");
             Console.ReadKey();
         }
     }
