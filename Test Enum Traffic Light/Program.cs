176d2e1cd434769a9e06e933e0b7628d55fd4a1a
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-12-15 17:00:12 +0100

    Projektdateien hinzufügen.

---
+++ Test Enum Traffic Light/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Test_Enum_Traffic_Light
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrafficLight light = new TrafficLight("Go");
+
+            Console.WriteLine("Traffic Light Action: " + light.getAction());
+            Console.WriteLine("Traffic Light Color: " + light.getColor());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test Enum Traffic Light/TrafficLight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Enum_Traffic_Light
+{
+    internal class TrafficLight
+    {
+        public enum LightColor
+        {
+            RED,
+            YELLOW,
+            GREEN
+        }
+
+        public string action;
+
+        public TrafficLight(string action)
+        {
+            this.action = action;
+        }
+
+        public string getAction()
+        {
+            return action;
+        }
+
+        public LightColor getColor()
+        {
+            switch (action)
+            {
+                case "Stop":
+                    return LightColor.RED;
+                case "Wait":
+                    return LightColor.YELLOW;
+                case "Go":
+                    return LightColor.GREEN;
+                default:
+                    return LightColor.RED;
+            }
+        }
+    }
+}
